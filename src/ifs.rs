46af76da05bb02bb6555e7a6ed6a05b63608f99d
narcolembia <masterlybeatle@gmail.com>
2025-10-25 08:11:52 -0700

    added more to gui, started to implement threaded renderer

--- src/ifs.rs
+++ src/ifs.rs
@@ -1,80 +1,98 @@
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::{
+    f64::consts::{PI, TAU},
+    sync::atomic::{AtomicU64, Ordering},
+};
 
-use num_complex::{Complex64};
-use std::f64::consts::{PI,TAU};
+use eframe::egui;
+use formulac::{compile, UserDefinedTable, Variables};
+use image::{Pixel, Rgba, RgbaImage};
+use num_complex::Complex64;
 use rand::prelude::*;
 use rand_distr::weighted::WeightedAliasIndex;
-
 use rayon;
-
-use image::{Pixel, Rgba, RgbaImage};
 
 type Complex = num_complex::Complex64;
 
+pub trait ComplexIfsFunction: (Fn(&Complex, &mut ThreadRng) -> Complex) + Sync {}
 
-pub trait ComplexIfsFunction: (Fn(&Complex, &mut ThreadRng) -> Complex) + Sync { 
+impl<Func: Fn(&Complex, &mut ThreadRng) -> Complex + Sync> ComplexIfsFunction for Func {}
 
-    
-}
-
-impl<Func: Fn(&Complex,&mut ThreadRng) -> Complex + Sync> ComplexIfsFunction for Func {
-
-}
-
-pub struct IfsHistogram{
-    frame:(usize,usize),
-    max:u64,
+pub struct IfsHistogram {
+    frame: (usize, usize),
+    max: u64,
     histogram: Box<[AtomicU64]>,
-
 }
 
-impl IfsHistogram{
-
-    pub fn new(frame:(usize,usize)) -> Self{
-        IfsHistogram { 
-            frame , 
-            max: 0, 
-            histogram: vec![0u64; frame.0*frame.1].into_iter().map(AtomicU64::new).collect() }
+impl IfsHistogram {
+    pub fn new(frame: (usize, usize)) -> Self {
+        IfsHistogram {
+            frame,
+            max: 0,
+            histogram: (0 .. (frame.0 * frame.1) as u64)
+                .into_iter()
+                .map(|_| AtomicU64::new(0))
+                .collect(),
+        }
     }
 
-    fn reset(&mut self){
-        for i in 0..self.frame.0*self.frame.1{
+    fn reset(&mut self) {
+        for i in 0 .. self.frame.0 * self.frame.1 {
             self.histogram[i].store(0, Ordering::Relaxed);
         }
     }
 
-    fn complex_to_2d_index(&self,z:&Complex)->Option<(usize,usize)>{
-        let index:Complex =((*z + Complex::new(1.0,1.0))/2.0) * self.frame.1 as f64;
-        if index.re >=self.frame.0 as f64|| index.re < 0.0 || index.im >= self.frame.1 as f64 || index.im < 0.0{
-            return None
+    fn complex_to_2d_index(&self, z: &Complex) -> Option<(usize, usize)> {
+        let index: Complex = ((*z + Complex::new(1.0, 1.0)) / 2.0) * self.frame.1 as f64;
+        if index.re >= self.frame.0 as f64 ||
+            index.re < 0.0 ||
+            index.im >= self.frame.1 as f64 ||
+            index.im < 0.0
+        {
+            return None;
+        } else {
+            return Some((index.re as usize, index.im as usize));
         }
-        else{return Some((index.re as usize, index.im as usize))}
     }
-    
-    fn index2d_to_index1d(&self,index:(usize,usize)) -> usize{
+
+    fn index2d_to_index1d(&self, index: (usize, usize)) -> usize {
         return index.0 as usize + (index.1 as usize * self.frame.0);
     }
 
-    pub fn iterate_ifs(&mut self,ifs:&Box<dyn '_+ ComplexIfsFunction >, transform: fn(&Complex) -> Complex, num_iters: u32,num_threads:u32,){
-        let iters_per_thread = (num_iters/num_threads) as usize;
-        let global_max:AtomicU64 = 0.into();
+    pub fn iterate_ifs(
+        &mut self,
+        ifs: &Box<dyn '_ + ComplexIfsFunction>,
+        rotate_scale: Complex,
+        translate: Complex,
+        num_iters: u64,
+        num_threads: u32,
+    ) {
+        let iters_per_thread = (num_iters / num_threads as u64) as usize;
+        let global_max: AtomicU64 = 0.into();
         self.reset();
 
         rayon::scope(|scope| {
             for _ in 0 .. num_threads {
-                scope.spawn(|_|{
+                scope.spawn(|_| {
                     let mut rng = rand::rng();
                     let mut z = Complex::new(0.0, 0.0);
                     let mut local_max = 0;
-                    for _ in 0..iters_per_thread{
-                        z = ifs(&z,&mut rng);
-                        
-                        match self.complex_to_2d_index(&transform(&z)){
+                    for _ in 0 .. iters_per_thread {
+                        z = ifs(&z, &mut rng);
+                        if z.is_nan() {
+                            z = Complex::new(0.0, 0.0);
+                            continue;
+                        }
+
+                        match self.complex_to_2d_index(&(rotate_scale * z + translate)) {
                             None => (),
                             Some(index) => {
-                                let count = self.histogram[self.index2d_to_index1d(index)].fetch_add(1,Ordering::SeqCst) + 1;
-                                if count > local_max { local_max = count;}
-                            }
+                                let count = self.histogram[self.index2d_to_index1d(index)]
+                                    .fetch_add(1, Ordering::SeqCst) +
+                                    1;
+                                if count > local_max {
+                                    local_max = count;
+                                }
+                            },
                         }
                     }
                     global_max.fetch_max(local_max, Ordering::SeqCst);
@@ -84,27 +102,52 @@ impl IfsHistogram{
         self.max = global_max.load(Ordering::SeqCst);
     }
 
-
-    pub fn to_image(&self,image:&mut RgbaImage,color:Rgba<u8>, bgcolor:Rgba<u8>,gamma:f32){
-
-        for (x,y,pixel) in image.enumerate_pixels_mut(){
-            let value  = self.histogram[self.index2d_to_index1d((x as usize,y as usize))].load(Ordering::SeqCst);
-            let value = ((( (value as f32) / (self.max as f32) )).powf(gamma) * color[3] as f32) as u8;
-
-            *pixel = bgcolor;
-            pixel.blend(&Rgba([color[0],color[1],color[2],value]))
+    pub fn update(&mut self,functions:&Vec<String>, weights: Vec<f32>, variables:&Variables, user_funcs:&UserDefinedTable, rotate_scale:Complex, translate: Complex,num_iters:u64,num_threads: u32){
+        let closures:Vec<Box<dyn Fn(&[Complex]) -> Complex + Sync>>  = functions.iter().map(|f|{
+            let o:Box<dyn Fn(&[Complex]) -> Complex + Sync> = Box::new(compile(f, &["z"], variables, user_funcs).unwrap());
+            return o;
         }
+        ).collect();
+        let ifs = ifs_from_closures(closures, weights);
+        self.iterate_ifs(&ifs, rotate_scale, translate, num_iters, num_threads);
     }
 
+    pub fn write_to_image(
+        &self,
+        image: &mut RgbaImage,
+        color: Rgba<u8>,
+        bgcolor: Rgba<u8>,
+        gamma: f64,
+        brightness: f64,
+        contrast: f64,
+        threshold: f64,
+    ) {
+        for (x, y, pixel) in image.enumerate_pixels_mut() {
+            let value = self.histogram[self.index2d_to_index1d((x as usize, y as usize))]
+                .load(Ordering::SeqCst);
+            let mut value = (value as f64) / (self.max as f64);
+            if value < threshold {
+                value = 0.0
+            }
+            value = contrast * (value - 0.5) + 0.5 + brightness;
+            value = value.clamp(0.0, 1.0);
+            value = value.powf(gamma) * color[3] as f64;
+
+            *pixel = bgcolor;
+            pixel.blend(&Rgba([color[0], color[1], color[2], value as u8]))
+        }
+    }
 }
 
-pub fn ifs_from_closures(closures:Vec< Box<dyn '_ + Fn(&[Complex]) -> Complex + Sync>>, weights: Vec<f32>) -> Box<dyn '_ + ComplexIfsFunction >{
+struct Transform {
+    translate: Complex,
+    scale: f64,
+}
+
+pub fn ifs_from_closures(
+    closures: Vec<Box<dyn '_ + Fn(&[Complex]) -> Complex + Sync>>,
+    weights: Vec<f32>,
+) -> Box<dyn '_ + ComplexIfsFunction> {
     let weighted_rng = WeightedAliasIndex::new(weights).unwrap();
-    return Box::new(move |z,rng| closures[weighted_rng.sample(rng)]([*z].as_slice()))
+    return Box::new(move |z, rng| closures[weighted_rng.sample(rng)]([*z].as_slice()));
 }
-
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 mod ifs;
 
 
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::{ops::RangeInclusive, str::FromStr, sync::atomic::{AtomicU64, Ordering}, thread, sync::mpsc};
+
+
+use std::f64::consts::{PI,TAU};
 
 use num_complex::{Complex64};
-
-use std::f32::consts::{PI,TAU};
-
 use formulac::{compile, variable::{UserDefinedTable, Variables}};
-
 use image::{Pixel, Rgba, RgbaImage};
+use eframe::{egui::{self, load::SizedTexture, Color32, ColorImage, TextureHandle, TextureOptions}, CreationContext};
+// use egui_extras;
 
-use crate::ifs::{ifs_from_closures, IfsHistogram};
-use eframe::{egui::{self, load::SizedTexture, ColorImage, TextureHandle, TextureOptions}, CreationContext};
-use egui_extras;
+use crate::ifs::{ifs_from_closures, ComplexIfsFunction, IfsHistogram};
 
 type Complex = num_complex::Complex64;
 
@@ -30,139 +29,376 @@ fn main() -> eframe::Result {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            let fs = vec!["a*z + (1-a)*exp(i*0*tau/3)".to_string(),"a*z + (1-a)*exp(i*1*tau/3)".to_string(),"a*z + (1-a)*exp(i*2*tau/3)".to_string()];
+            let fs = vec!["tan(a*z + (1-a)*exp(i*0*TAU/3))".to_string(),"tan(a*z + (1-a)*exp(i*1*TAU/3))".to_string(),"tan(a*z + (1-a)*exp(i*2*TAU/3))".to_string()];
 
             Ok(Box::<MyApp>::new(MyApp::new(
                 cc,
                 fs,
-                vec![("a".to_string(),[0.0,0.0])],
-                (500,500),
-                Transform { translate: (0.0,0.0), scale: 1.0 },
-                ColoringParams { gamma: 1.0, color: egui::Rgba::from_rgba_unmultiplied(1.0, 1.0, 1.0, 1.0), bg_color: egui::Rgba::from_rgba_unmultiplied(0.0,0.0,0.0,1.0) },
-                10000,
-                10
+                vec![("a".to_string(),[SliderData::default(),SliderData::default()])],
+                (1000,1000),
+                Transform { 
+                    translate: (SliderData::new(0.0,-1.0,1.0),SliderData::new(0.0,-1.0,1.0)),
+                    scale: SliderData::new(1.0,0.0,1.0),
+                    rotate: SliderData::new(0.0,0.0,1.0) },
+                ColoringParams { 
+                    gamma: SliderData::new(1.0,0.0,2.0),
+                    threshold: SliderData::new(0.0,0.0,1.0),
+                    brightness:SliderData::new(0.0, 0.0, 1.0 ),
+                    contrast: SliderData::new(1.0, 0.0, 2.0 ),
+                    color: egui::Rgba::from_rgba_unmultiplied(1.0, 1.0, 1.0, 1.0),
+                    bg_color: egui::Rgba::from_rgba_unmultiplied(0.0,0.0,0.0,1.0) },
+                1000000,
+                1000000,
+                100000,
+                (2000,2000),
+                10,
+
             ))
         )}
     ))
 }
 struct Transform{
-    translate:(f32,f32),
-    scale:f32,
-
+    translate:(SliderData,SliderData),
+    scale:SliderData,
+    rotate:SliderData,
 }
 
 struct ColoringParams{
-    gamma:f32,
+    gamma:SliderData,
+    brightness:SliderData,
+    contrast:SliderData,
+    threshold:SliderData,
     color:egui::Rgba,
     bg_color:egui::Rgba,
+    
 }
+
+#[derive(Clone)]
+struct SliderData{
+    val: f64,
+    min: TextBoxData<f64>,
+    max: TextBoxData<f64>
+}
+
+#[derive(Clone)]
+struct TextBoxData<T: ToString + FromStr+Clone>{
+    text: String,
+    value: T,
+}
+
+impl Default for TextBoxData<f64>{
+    fn default() -> Self {
+        TextBoxData::new(0.0)
+    }
+}
+impl<T: ToString + FromStr + Clone> TextBoxData<T>{
+    fn new(value: T) -> Self{
+        TextBoxData{
+            text: value.to_string(),
+            value,
+        }
+    }
+
+    fn update(&mut self){
+        match self.text.parse::<T>(){
+            Ok(value)=> self.value = value,
+            Err(_) =>(),
+        }
+    }
+}
+
+
+impl SliderData{
+    fn new(val:f64,min:f64,max:f64) -> Self{
+        SliderData {val, min:TextBoxData::new(min), max:TextBoxData::new(max) }
+    }
+    fn update(&mut self){
+        self.min.update();
+        self.max.update()
+    }
+}
+impl Default for SliderData{
+    fn default() -> Self {
+        SliderData { val: 0.0, min: TextBoxData::new(0.0), max: TextBoxData::new(1.0) }
+    }
+}
+
+
 
 struct MyApp {
 
     functions: Vec<String>,
-    variables_vec:Vec<(String,[f32;2])>,
+    variables_vec:Vec<(String,[SliderData;2])>,
     variables_table:Variables,
     user_funcs:UserDefinedTable,
-    weights: Vec<f32>,
+    weights: Vec<SliderData>,
 
     frame: (usize,usize),
-    histogram:IfsHistogram,
+    preveiw_histogram: IfsHistogram,
+    export_histogram: IfsHistogram,
+
     render_preveiw: RgbaImage,
     texture_handle: TextureHandle,
 
     transform: Transform,
 
-    coloring_params:ColoringParams,
+    coloring_params: ColoringParams,
 
-    num_iters: u32,
-    num_threads:u32,
+    num_iters: TextBoxData<u64>,
+    high_res_snapshot_iters: TextBoxData<u64>,
+    num_threads: u32,
+
+    export_num_iters: TextBoxData<u64>,
+    export_resolution: (TextBoxData<usize>,TextBoxData<usize>),
     
-   
+    export_flag:bool,
+    high_res_snapshot_flag: bool,
+    update_histogram_flag: bool,
+    redraw_flag: bool,
+    disable_ui_flag: bool,
+
+    worker_thread: thread::JoinHandle<()>,
+    job_sender: mpsc::Sender<RenderJob>,
+    job_result_receiver: mpsc::Receiver<IfsHistogram>,
 }
 
 impl MyApp{
-    pub fn new(cc:&CreationContext,functions:Vec<String>, variables_vec:Vec<(String,[f32;2])>, frame:(usize,usize), transform:Transform, coloring_params:ColoringParams, num_iters:u32, num_threads:u32 ) -> Self{
-        
+    pub fn new(
+        cc:&CreationContext,functions:Vec<String>,
+        variables_vec:Vec<(String,[SliderData;2])>,
+        frame:(usize,usize),
+        transform:Transform,
+        coloring_params:ColoringParams,
+        num_iters:u64, high_res_snapshot_iters:u64,
+        export_num_iters:u64,export_resolution:(usize,usize), 
+        num_threads:u32
+        ) -> Self{
+        let len = functions.len();
+        let (job_sender, job_receiver) = mpsc::channel(); // FIXME: `crossbeam_channel::bounded(1)`
+        let (job_result_sender, job_result_receiver) = mpsc::channel(); // FIXME: ditto
         MyApp{
             functions,
             variables_vec,
             user_funcs: UserDefinedTable::new(),
             variables_table: Variables::new(),
-            weights: Vec::new(),
+            weights: vec![SliderData::new(1.0, 0.0, 1.0);len],
 
             frame,
-            histogram: IfsHistogram::new(frame),
+            preveiw_histogram: IfsHistogram::new(frame),
+            export_histogram: IfsHistogram::new(export_resolution),
 
-            render_preveiw: RgbaImage::new(frame.0 as u32, frame.1 as u32),
+            render_preveiw: RgbaImage::new(frame.0 as u32,frame.1 as u32),
             texture_handle: cc.egui_ctx.load_texture("render_preveiw",egui::ColorImage::example(),TextureOptions::default() ),
 
             transform,
             coloring_params,
 
-            num_iters,
+            num_iters: TextBoxData::new(num_iters),
+            high_res_snapshot_iters: TextBoxData::new(high_res_snapshot_iters),
             num_threads,
-            
 
+            export_num_iters: TextBoxData::new(export_num_iters),
+            export_resolution: (TextBoxData::new(export_resolution.0),TextBoxData::new(export_resolution.1)),
 
+            update_histogram_flag: true,
+            redraw_flag:true,
+            disable_ui_flag:false,
+
+            worker_thread: thread::spawn(move || worker_thread(job_receiver, job_result_sender)),
+            job_sender,
+            job_result_receiver,
         }
     }
-    fn update_histogram(&mut self){
-        let closures:Vec<Box<dyn Fn(&[Complex]) -> Complex + Sync>>  = self.functions.iter().map(|f|{
-            let o:Box<dyn Fn(&[Complex]) -> Complex + Sync> = Box::new(compile(f, &["z"], &self.variables_table, &self.user_funcs).unwrap());
-            return o;
-        }
-        ).collect();
-        let ifs = ifs_from_closures(closures, self.weights.clone());
-        let transform = |z:&Complex|*z*0.5;
-        self.histogram.iterate_ifs(&ifs, transform, self.num_iters, self.num_threads);
+
+    fn get_weights(&self) -> Vec<f32>{
+        return self.weights.iter().map(|w| w.val as f32).collect()
     }
+
+    fn get_rotate_scale(&self) -> Complex{
+        return self.transform.scale.val * Complex::cis(self.transform.rotate.val)
+    }
+    
+    fn get_translate(&self) -> Complex{
+        return Complex::new(self.transform.translate.0.val,self.transform.translate.1.val)
+    }
+
+    fn get_export_resolution(&self) -> (usize,usize){
+        return (self.export_resolution.0, self.export_resolution.1)
+    }
+    
+   
+    
 }
 
 impl eframe::App for MyApp {
+    
+    
+    
+    
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        
+        fn make_slider(ui: &mut egui::Ui, data: &mut SliderData, name: String, set_hist_update:Option<&mut bool>, set_redraw:Option<&mut bool>){
+            let mut set_updates = false;
+            ui.horizontal(|ui|{
+                ui.add(egui::Label::new(name));
+                if ui.add(egui::Slider::new(&mut data.val,data.min.value..=data.max.value)).changed(){
+                    set_updates = true;
+                }
+                if ui.add(egui::TextEdit::singleline(&mut data.min.text).desired_width(100.0)).lost_focus() { 
+                    data.update();
+                    set_updates = true;
+                };
+                if ui.add(egui::TextEdit::singleline(&mut data.max.text).desired_width(100.0)).lost_focus() { 
+                    data.update();
+                    set_updates = true;
+                };
+    
+            });
+            if set_updates{
+                match set_hist_update{
+                    Some(flag)=> *flag = true,
+                        None => ()
+                    }
+                match set_redraw{
+                    Some(flag)=> *flag = true,
+                        None => ()
+                    }
+            } 
+        }
 
+        fn make_textbox<T: ToString + FromStr + Clone>(ui: &mut egui::Ui, data: &mut TextBoxData<T>, name: String, set_hist_update:Option<&mut bool>, set_redraw:Option<&mut bool>){
+            ui.horizontal(|ui|{
+                ui.add(egui::Label::new(name));
+                if ui.add(egui::TextEdit::singleline(&mut data.text)).lost_focus() { 
+                    data.update();
+                    match set_hist_update{
+                        Some(flag)=> *flag = true,
+                        None => ()
+                    }
+                    match set_redraw{
+                        Some(flag)=> *flag = true,
+                        None => ()
+                    }
+                };
+            });
+          
+        }
+
+         
+       
+       
         
-        
-        self.update_histogram();
-        self.histogram.to_image(&mut self.render_preveiw, Rgba(self.coloring_params.color.to_srgba_unmultiplied()), Rgba(self.coloring_params.bg_color.to_srgba_unmultiplied()), self.coloring_params.gamma);
-        let color_image = ColorImage::from_rgba_unmultiplied([self.frame.0, self.frame.1], &self.render_preveiw);
-        self.texture_handle.set(color_image, TextureOptions::default());
-        let sized_texture = egui::load::SizedTexture::new(&self.texture_handle, egui::vec2(self.frame.0 as f32, self.frame.1 as f32));
         egui::SidePanel::left("test_side_panel").show(ctx,|ui|{
-            ui.add(egui::Slider::new(&mut self.transform.scale,0.0..=2.0))
-        });
 
+            if self.disable_ui_flag { ui.disable();}
+            
+            ui.add(egui::Label::new("Transform"));
+            make_slider(ui, &mut self.transform.translate.0, "translate x".to_string(), Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+            make_slider(ui, &mut self.transform.translate.1, "translate y".to_string(), Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+            make_slider(ui, &mut self.transform.scale, "scale".to_string() , Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+            make_slider(ui, &mut self.transform.rotate, "rotate".to_string(), Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+
+            ui.add(egui::Separator::default());
+
+
+            ui.add(egui::Label::new("Variables"));
+            for (name,data) in self.variables_vec.iter_mut(){
+
+                make_slider(ui, &mut data[0], name.to_string() + ": magnitude", Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+                make_slider(ui, &mut data[1], name.to_string() + ": phase          ", Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+                let z = data[0].val*Complex::cis(data[1].val*TAU);
+                self.variables_table.insert(&[(name.as_str(),z)]);
+            }
+            ui.add(egui::Separator::default());
+
+            ui.add(egui::Label::new("weights"));
+            for (index, weight) in self.weights.iter_mut().enumerate(){
+                make_slider(ui, weight, index.to_string(), Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+            }
+            ui.add(egui::Separator::default());
+
+            ui.add(egui::Label::new("color"));
+            
+            make_slider(ui, &mut self.coloring_params.gamma, "gamma".to_string(), None, Some(&mut self.redraw_flag));
+            make_slider(ui, &mut self.coloring_params.brightness, "brightness".to_string(), None, Some(&mut self.redraw_flag));
+            make_slider(ui, &mut self.coloring_params.contrast, "contrast".to_string(), None, Some(&mut self.redraw_flag));
+            make_slider(ui, &mut self.coloring_params.threshold, "threshold".to_string(), None, Some(&mut self.redraw_flag));
+            if egui::color_picker::color_edit_button_rgba(ui, &mut self.coloring_params.color, egui::color_picker::Alpha::BlendOrAdditive).changed(){ self.redraw_flag = true;};
+            if egui::color_picker::color_edit_button_rgba(ui, &mut self.coloring_params.bg_color, egui::color_picker::Alpha::BlendOrAdditive).changed(){ self.redraw_flag = true;};
+            ui.add(egui::Separator::default());
+            
+            ui.add(egui::Label::new("iteration settings"));
+            make_textbox(ui, &mut self.num_iters, "num_iters".to_string(), Some(&mut self.update_histogram_flag), Some(&mut self.redraw_flag));
+            make_textbox(ui, &mut self.high_res_snapshot_iters, "snapshot iters".to_string(), None, None);
+            if ui.button("snapshot").clicked(){
+                self.update_histogram_flag = true;
+                self.high_res_snapshot_flag = true;
+                self.redraw_flag = true;
+            }
+            
+
+            ui.add(egui::Label::new("export settings"));
+            make_textbox(ui, &mut self.export_num_iters, "export iteres".to_string(), None, None);
+
+            ui.horizontal(|ui|{
+                ui.add(egui::Label::new("export resolution"));
+                make_textbox(ui, &mut self.export_resolution.0, "".to_string(), None, None);
+                make_textbox(ui, &mut self.export_resolution.1, "".to_string(), None, None);
+                
+            });
+            
+            
+        });
+        //println!("updated variables");
+
+        
+        
+        if self.update_histogram_flag{
+            let iters = if self.high_res_snapshot_flag = true {self.high_res_snapshot_iters.value} else {self.num_iters.value};
+            self.preveiw_histogram.update(&self.functions,self.get_weights(),&self.variables_table,&self.user_funcs,self.get_rotate_scale(),self.get_translate(),iters,self.num_threads);
+            self.update_histogram_flag = false;
+        }
+        if self.redraw_flag{
+            self.preveiw_histogram.write_to_image(
+                &mut self.render_preveiw,
+                Rgba(self.coloring_params.color.to_srgba_unmultiplied()),
+                Rgba(self.coloring_params.bg_color.to_srgba_unmultiplied()),
+                self.coloring_params.gamma.val,
+                self.coloring_params.brightness.val,
+                self.coloring_params.contrast.val,
+                self.coloring_params.threshold.val,
+                );
+            //println!("generated image");
+            let color_image = ColorImage::from_rgba_unmultiplied([self.frame.0, self.frame.1], &self.render_preveiw);
+            self.texture_handle.set(color_image, TextureOptions::default());
+            self.redraw_flag = false;
+        }
+        if self.export_flag{
+            self.export_histogram = IfsHistogram::new(self.export_resolution.0)
+        }
+        //println!("updated histogram");
+       
+        
+        let sized_texture = egui::load::SizedTexture::new(&self.texture_handle, egui::vec2(self.frame.0 as f32, self.frame.1 as f32));
         egui::CentralPanel::default().show(ctx, |ui| {
             egui::ScrollArea::both().show(ui, |ui| {
-                ui.image(sized_texture);
+                ui.image(sized_texture); 
             });
         });
     }
     
 }
-/* 
-fn main() { 
-    let vars = Variables::new();
-    let funcs = UserDefinedTable::new();
 
-    let mut fs:Vec<Box<dyn Fn(&[Complex]) -> Complex + Sync>> = Vec::new();
-    fs.push(Box::new(compile("0.5*z + 0.5*exp(0*i*TAU/3)", &["z"], &vars, &funcs).unwrap()));
-    fs.push(Box::new(compile("0.5*z + 0.5*exp(1*i*TAU/3)", &["z"], &vars, &funcs).unwrap()));
-    fs.push(Box::new(compile("0.5*z + 0.5*exp(2*i*TAU/3)", &["z"], &vars, &funcs).unwrap()));
-  
-
-    let fs = ifs_from_closures(fs, vec![1.0,1.0,1.0]);
-
-    let mut hist = IfsHistogram::new((2000,2000));
-
-    let transform = |z:&Complex| *z;
-
-    hist.iterate_ifs(&fs, transform, 10usize.pow(5),5);
-
-    let img = hist.to_image(Rgba([255,255,255,255]), Rgba([0,0,0,255]));
-    img.save("output.png");
-
+struct RenderJob {
+    histogram: IfsHistogram,
+    // TODO
 }
 
-
-*/
+fn worker_thread(jobs: mpsc::Receiver<RenderJob>, job_results: mpsc::Sender<IfsHistogram>) {
+    for job in jobs.iter() {
+        let RenderJob { mut histogram, .. } = job;
+        job_results.send(histogram).unwrap
+   }
+j =  .. ,margotsih tum {} 
+   }
+}
